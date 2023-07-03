e82ea672a5dd43d1a93dc3e0629cc18367969d7f
ahmad <ahmad_mahmoud@gmail.com>
2023-07-03 09:55:12 +0300

    init commit, install breeze and integrate front design in project

---
+++ resources/views/front/auth-master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+
+<html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default"
+    data-assets-path="../assets/" data-template="vertical-menu-template-free">
+
+@include('front.partials.auth.head')
+
+
+
+<body>
+    <!-- Content -->
+
+    @yield('content')
+
+    <!-- / Content -->
+
+    @include('front.partials.auth.scripts')
+
+</body>
+
+</html>
